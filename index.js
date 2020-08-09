821553b6234271a9519371e99b87e8b9edf698ed
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-08-09 21:15:37 +0530

    publish npm module (#2)
    * package
    
    * rm package-lock.json
    
    * Update index.js
    
    * Update index.js
    
    * Update README.md
    
    Co-authored-by: Salmandabbakuti <Salmandabbakuti@github.com>

---
+++ index.js
@@ -0,0 +1,42 @@
+var axios = require("axios");
+
+const get = async(url, dataObj) => {
+try{
+const res = await axios.get(url,{
+    params: dataObj
+ })
+return {err:undefined, response: res}
+}catch(err){
+return {err:err, response: undefined}
+   }
+}
+
+/*
+const {err, response} = await get("http://localhost:8000/search", {queryId:"cars"})
+if(err) {
+console.log(`it returned error: ${err}`)
+else{
+console.log(`it returned response: ${response}`)
+*/
+const post = async(url, dataObj) => {
+    try{
+    let res = await axios.post(url, dataObj);
+    //console.log(res)
+     return {err:undefined, response:res}
+    }catch(err){
+     return {err:err, response:undefined}
+    }
+}
+
+/*
+const {err, response} = await post("http://localhost:8000/login", {username:"salman", password:"salman12345"})
+if(err) {
+console.log(`it returned error: ${err}`)
+}else{
+ console.log(`it returned response: ${response}`)
+}
+*/
+module.exports = {
+    post,
+    get
+    }
